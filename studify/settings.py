bb1bd5e2ecbe3821ed31c5fed6e55d1866dcbab1
Rawat <tuhinr@in.startv.com>
2019-09-23 15:27:54 +0530

    created templates folder and home.html page

--- studify/settings.py
+++ studify/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'studify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['studify/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
