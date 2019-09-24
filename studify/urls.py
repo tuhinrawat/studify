345d6723dd389d8633b13305269b5a35f50d5210
Tuhin Rawat <tuhinrawat@gmail.com>
2019-09-25 01:39:35 +0530

    Home Page updated

--- studify/urls.py
+++ studify/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from .import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('',views.home)
 ]
+urlpatterns+=staticfiles_urlpatterns()
